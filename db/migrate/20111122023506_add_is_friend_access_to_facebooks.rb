42874e41a546e1e56ad7922fce78ae75195eebc5
Edward Mehr <s.r.mehr@gmail.com>
2011-11-22 00:14:17 -0800

    changing migrations

--- db/migrate/20111122023506_add_is_friend_access_to_facebooks.rb
+++ db/migrate/20111122023506_add_is_friend_access_to_facebooks.rb
@@ -1,9 +1,6 @@
 class AddIsFriendAccessToFacebooks < ActiveRecord::Migration
   def up
     add_column :facebooks, :is_friend_access, :boolean
-    Facebook.find(:all).each do |f|
-      f.update_attribute :is_friend_access, false
-    end
   end
   
   def down
