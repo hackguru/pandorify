236bb07a7efb5ef2e5e64651d82612938347a282
Edward Mehr <s.r.mehr@gmail.com>
2011-11-22 02:30:46 -0800

    changing migrations

--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -63,7 +63,10 @@ class Facebook < ActiveRecord::Base
       _fb_user_.name = fb_user.name
       _fb_user_.save!
       friends = current_user.friends #helps perf
+      puts "FOUND IT: Gabe Audick " + _fb_user_.name if fb_user.name == "Gabe Audick"
       current_user.friends << _fb_user_ if !friends.include? _fb_user_
+      puts "FOUND IT: Gabe Audick " + _fb_user_.name if fb_user.name == "Gabe Audick"
+      puts current_user.friends.to_s
       _fb_user_    
     end
   end
