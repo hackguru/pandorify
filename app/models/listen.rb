de51a6031506276d16f8c5a64f44074aa66020f1
Edward Mehr <s.r.mehr@gmail.com>
2012-01-30 22:19:16 -0800

    playing with fb graph

--- app/models/listen.rb
+++ app/models/listen.rb
@@ -6,5 +6,5 @@ class Listen < ActiveRecord::Base
         :select => "#{Song.table_name}.*, count(#{Listen.table_name}.id) as listen_count",
         :joins => "LEFT JOIN #{Song.table_name} ON #{Listen.table_name}.song_id = #{Song.table_name}.id",
         :group => "#{Song.table_name}.id, #{Song.table_name}.identifier, #{Song.table_name}.title, #{Song.table_name}.url, #{Song.table_name}.created_at, #{Song.table_name}.updated_at, #{Song.table_name}.application_id",
-        :order => "listen_count DESC"  
+        :order => "listen_count DESC, #{Song.table_name}.popularity DESC"  
 end
