9e6bc60c706a7fcec3d93ab7440da9ce52edba66
Edward Mehr <s.r.mehr@gmail.com>
2012-01-30 17:28:55 -0800

    checking action fb-graph

--- app/models/listen.rb
+++ app/models/listen.rb
@@ -3,7 +3,7 @@ class Listen < ActiveRecord::Base
   belongs_to :song
   
   scope :sorted_based_on_song,
-        :select => "#{Listen.table_name}.id, count(#{Listen.table_name}.id) as listen_count",
+        :select => "#{Song.table_name}.*, count(#{Listen.table_name}.id) as listen_count",
         :group => "#{Listen.table_name}.song_id",
         :order => "listen_count DESC"  
 end
