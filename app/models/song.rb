4d518085a9be9a2677e95daf60b58a851e393c0c
Edward Mehr <s.r.mehr@gmail.com>
2012-01-30 18:52:43 -0800

    playing with fb graph

--- app/models/song.rb
+++ app/models/song.rb
@@ -18,11 +18,11 @@ class Song < ActiveRecord::Base
       new_artist_url = new_info['track']['artists'][0]['href'].sub("spotify:artist:","http://open.spotify.com/artist/").strip
       
       new_album = Album.find_or_create_by_url(new_album_url)
-      new_album.name = new_info['track']['album']['name'][0..255] #only the first characters - might wanna change this later to text
+      new_album.name = new_info['track']['album']['name'][0..254] #only the first characters - might wanna change this later to text
       self.album = new_album
       
       new_artist = Artist.find_or_create_by_url(new_artist_url)
-      new_artist.name = new_info['track']['artists'][0]['name'][0..255]  #only the first characters - might wanna change this later to text
+      new_artist.name = new_info['track']['artists'][0]['name'][0..254]  #only the first characters - might wanna change this later to text
       self.artist = new_artist
       
       self.save!
