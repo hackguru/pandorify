1218c93dbe91ef48c4e432ce555dec112bdfad8b
Edward Mehr <s.r.mehr@gmail.com>
2012-01-30 17:41:25 -0800

    playing with fb graph

--- app/models/listen.rb
+++ app/models/listen.rb
@@ -5,6 +5,6 @@ class Listen < ActiveRecord::Base
   scope :sorted_based_on_song,
         :select => "#{Song.table_name}.*, count(#{Listen.table_name}.id) as listen_count",
         :joins => "LEFT JOIN #{Song.table_name} ON #{Listen.table_name}.song_id = #{Song.table_name}.id",
-        :group => "#{Song.table_name}.identifier, #{Song.table_name}.title, #{Song.table_name}.url, #{Song.table_name}.created_at, #{Song.table_name}.updated_at, #{Song.table_name}.application_id",
+        :group => "#{Song.table_name}.id, #{Song.table_name}.identifier, #{Song.table_name}.title, #{Song.table_name}.url, #{Song.table_name}.created_at, #{Song.table_name}.updated_at, #{Song.table_name}.application_id",
         :order => "listen_count DESC"  
 end
