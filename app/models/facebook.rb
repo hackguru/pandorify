3ff72bb74e2268b92f13b43becbb25eda803820f
Edward Mehr <s.r.mehr@gmail.com>
2012-02-02 21:32:43 -0800

    updating recommendations

--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -178,6 +178,7 @@ class Facebook < ActiveRecord::Base
     list.each do |obj|
       break if obj[1] == 0      
       number_of_songs = (obj[1].to_f/sum.to_f).to_i
+      puts 'obj1 : ' + obj[1].to_s + 'sum : ' + sum.to_s
       puts 'number_of_songs : ' + number_of_songs.to_s
       # list_of_songs = Song.song_based_on_sorted_listens_by_user(obj[0])
       # list_of_songs.each do |song|
@@ -187,7 +188,7 @@ class Facebook < ActiveRecord::Base
       #   else
       #     recom = Recommendation.create(:facebook => self, :song => song, :common_rank => obj[1])
       #     recom.save!
-      #     number_of_songs -= 0
+      #     number_of_songs -= 1
       #   end
       # end
     end
