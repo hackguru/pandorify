3d386fd6f6f993f40cc67c76a276799d95f89c7b
Edward Mehr <s.r.mehr@gmail.com>
2012-02-02 23:39:26 -0800

    updating recommendations

--- app/models/song.rb
+++ app/models/song.rb
@@ -86,9 +86,9 @@ class Song < ActiveRecord::Base
       list.each do |obj|
         # begin
           c = Curl::Easy.perform("http://tinysong.com/b/#{CGI.escape(obj.title.to_s + ' ' + obj.artist.name.to_s)}?format=json&key=186bd60f3a33be26da02d62d334bddf4") # FROM Tinysong
-          # parsed_json = ActiveSupport::JSON.decode(c.body_str)
-          puts c.body_str
-          # obj.tiny_song = parsed_json['SongID']
+          parsed_json = ActiveSupport::JSON.decode(c.body_str)
+          puts parsed_json['SongID']
+          #obj.tiny_song = parsed_json['SongID']
           # obj.save!          
         # rescue
         #   next
