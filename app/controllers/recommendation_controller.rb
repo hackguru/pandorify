a381e5b92dd5ff01fe8d6bb7908f487cb929e143
Edward Mehr <s.r.mehr@gmail.com>
2012-02-04 09:20:18 -0800

    adding duration to songs

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -2,7 +2,11 @@ class RecommendationController < ApplicationController
 
   def recommended
     @page =  params[:page] || 1
-    @songs = current_user.recommendations.paginate(:page => @page )
+    @recoms = current_user.recommendations.paginate(:page => @page )
+    @songs = []
+    @recoms.each do |recom|
+      @songs << recom.song
+    end
     respond_to do |format|
        format.js
     end
