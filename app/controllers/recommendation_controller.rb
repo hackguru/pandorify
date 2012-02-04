0120456fe49061b06c6d7d062393ae157c63b01c
Edward Mehr <s.r.mehr@gmail.com>
2012-02-04 00:55:30 -0800

    adding playlist names

--- app/controllers/recommendation_controller.rb
+++ app/controllers/recommendation_controller.rb
@@ -1,7 +1,5 @@
 class RecommendationController < ApplicationController
 
-  self.per_page = 20
-
   def recommended
     @page =  params[:page] || 1
     @songs = current_user.recommendations.paginate(:page => @page )
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,4 +2,7 @@ class Recommendation < ActiveRecord::Base
   belongs_to :facebook
   belongs_to :song
   belongs_to :recommended_by, :class_name => 'Facebook'
+  
+  self.per_page = 20
+  
 end
