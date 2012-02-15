841f311d2f66b10093d9176cbf2c60a5752a6a90
Edward Mehr <s.r.mehr@gmail.com>
2012-02-14 17:27:03 -0800

    only update pic if its not there

--- app/models/song.rb
+++ app/models/song.rb
@@ -49,7 +49,7 @@ class Song < ActiveRecord::Base
         :select => "#{Song.table_name}.*, count(DISTINCT #{Facebook.table_name}.id) as user_count",
         :joins => "JOIN #{Listen.table_name} ON #{Song.table_name}.id = #{Listen.table_name}.song_id JOIN #{Facebook.table_name} ON #{Listen.table_name}.facebook_id = #{Facebook.table_name}.id AND #{users_string}",
         :group => "#{Song.table_name}.id, #{Song.table_name}.identifier, #{Song.table_name}.title, #{Song.table_name}.url, #{Song.table_name}.created_at, #{Song.table_name}.updated_at, #{Song.table_name}.application_id, #{Song.table_name}.popularity, #{Song.table_name}.artist_id, #{Song.table_name}.album_id, #{Song.table_name}.tiny_song,#{Song.table_name}.duration",
-        :order => "user_count DESC, coalesce(#{Song.table_name}.popularity, 0) DESC"
+        :order => "user_count DESC, count(DISTINCT #{Listen.table_name}.id) DESC, coalesce(#{Song.table_name}.popularity, 0) DESC"
   }}
 
   scope :not_common_songs, lambda { |*args| {
