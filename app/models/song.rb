bc000c0557e8efaf6e74d46097d3c828b0d95f3b
Edward Mehr <s.r.mehr@gmail.com>
2012-02-20 14:03:04 -0800

    new api for echo nest

--- app/models/song.rb
+++ app/models/song.rb
@@ -122,7 +122,7 @@ class Song < ActiveRecord::Base
     response = http.request(request).body
     header = http.request(request).header
     new_info = JSON.parse(response)
-    puts new_info.to_s
+    puts response.to_s
     puts header.to_s
   end
   
