73c288ab03d3bf063138f216115c2a2c8671da40
Edward Mehr <s.r.mehr@gmail.com>
2012-02-22 15:17:38 -0800

    removing migration

--- db/migrate/20120222220849_add_temp_to_songs.rb
+++
@@ -1,9 +0,0 @@
-class AddTempToSongs < ActiveRecord::Migration
-  def change
-    add_column :songs, :temp, :text
-
-    Song.reset_column_information
-    Song.find_each { |s| s.update_attribute(:temp, s.title) }
-
-  end
-end
