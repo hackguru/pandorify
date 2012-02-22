c89a64e229305f833b8302f5411736a2aa77b031
Edward Mehr <s.r.mehr@gmail.com>
2012-02-22 14:10:38 -0800

    Adding temp to songs to change column types

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120222034758) do
+ActiveRecord::Schema.define(:version => 20120222220849) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(:version => 20120222034758) do
   create_table "parties", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "host_id"
   end
 
   create_table "playlists", :force => true do |t|
