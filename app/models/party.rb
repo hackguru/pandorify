e8f19e997da921bda1c6422459fe271bd4f753ee
Edward Mehr <s.r.mehr@gmail.com>
2012-02-21 20:11:21 -0800

    adding parties to db

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,4 @@
 class Party < ActiveRecord::Base
-  has_and_belongs_to_many :facebooks
-  has_and_belongs_to_many :songs
+  has_and_belongs_to_many :facebooks, :join_table => "facebooks_parties"
+  has_and_belongs_to_many :songs, :join_table => "songs_parties"
 end
