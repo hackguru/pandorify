e737d155c32a1ee9e7989d19d9ce7751f646bce3
Edward Mehr <s.r.mehr@gmail.com>
2012-02-22 16:46:41 -0800

    associations for song request

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,6 +1,6 @@
 class Party < ActiveRecord::Base
   has_and_belongs_to_many :facebooks, :join_table => "facebooks_parties"
   has_many :songs, :through => :requestedsongs, :source => :song
-  has_many :requestedsongs
+  has_many :requestedsongs, :dependent => :destroy
   belongs_to :host, :foreign_key => :host_id, :class_name => 'Facebook'
 end
