040cf926207fa73106e88fd3ae26e0a869acada3
Edward Mehr <s.r.mehr@gmail.com>
2012-02-22 20:28:59 -0800

    adding twilio

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -19,18 +19,20 @@ class TwilioController < ApplicationController
   # {"AccountSid"=>"AC5cb9a6dcffb746ada26419b0b9621989", "Body"=>"test", "ToZip"=>"92677", "FromState"=>"CA", "ToCity"=>"LAGUNA NIGUEL", "SmsSid"=>"SM7ac62fadfde51b614605c1e830c9d395", "ToState"=>"CA", "To"=>"+19492720608", "ToCountry"=>"US", "FromCountry"=>"US", "SmsMessageSid"=>"SM7ac62fadfde51b614605c1e830c9d395", "ApiVersion"=>"2010-04-01", "FromCity"=>"IRVINE", "SmsStatus"=>"received", "From"=>"+19492664898", "FromZip"=>"92606", "controller"=>"call", "action"=>"sms"}
   def sms
     body = params["Body"].downcase.strip
-    # number = params["From"]
-    # user = Facebook.find_by_cell(number)
-    # party = user.parties.first
-    # 
-    # if party
+    number = params["From"]
+    user = Facebook.find_by_cell(number)
+    party = user.parties.first
+    
+    if party
       url = "http://ws.spotify.com/search/1/track.json?q=#{CGI.escape(body)}"
       uri = URI.parse(url)
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
       response = http.request(request).body
-      @new_info = JSON.parse(response)
-    # end
+      new_info = JSON.parse(response)
+      song = Song.find_by_url(Song.get_url_from_uri(@new_info["tracks"][0]["href"]))
+      Requestedsong.create(:facebook => user, :song => song, :party => part, :added => false)
+    end
 
     # @client = Twilio::REST::Client.new ACCOUNT_SID, ACCOUNT_TOKEN
     # @client.account.sms.messages.create(
@@ -42,7 +44,7 @@ class TwilioController < ApplicationController
     respond_to do |format|
        # format.js
        format.json{
-         render :json => Song.find_by_url(Song.get_url_from_uri(@new_info["tracks"][0]["href"]))        
+         render :json =>         
        }
     end
     
