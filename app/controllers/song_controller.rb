c43442c457bdaecba3690aa91211d0f907d3c263
Edward Mehr <s.r.mehr@gmail.com>
2012-02-22 18:11:41 -0800

    fixing song controller

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -59,5 +59,12 @@ class SongController < ApplicationController
     end
   end
   
+  private
+  
+  def str_to_bool
+    return true if self == true || self =~ (/(true|t|yes|y|1)$/i)
+    return false if self == false || self.blank? || self =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{self}\"")
+  end
   
 end
