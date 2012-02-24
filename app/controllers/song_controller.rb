4acf0726361a722a4c43be8119f3e07d4a5a82fe
Edward Mehr <s.r.mehr@gmail.com>
2012-02-23 19:12:58 -0800

    fixing errors

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -37,13 +37,5 @@ class SongController < ApplicationController
        format.js
     end
   end
-    
-  private
-  
-  def str_to_bool (arg)
-    return true if arg == true || arg =~ (/(true|t|yes|y|1)$/i)
-    return false if arg == false || arg.blank? || arg =~ (/(false|f|no|n|0)$/i)
-    raise ArgumentError.new("invalid value for Boolean: \"#{arg}\"")
-  end
-  
+      
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   include Authentication::HelperMethods
+  
+  def str_to_bool (arg)
+    return true if arg == true || arg =~ (/(true|t|yes|y|1)$/i)
+    return false if arg == false || arg.blank? || arg =~ (/(false|f|no|n|0)$/i)
+    raise ArgumentError.new("invalid value for Boolean: \"#{arg}\"")
+  end
+
 end
