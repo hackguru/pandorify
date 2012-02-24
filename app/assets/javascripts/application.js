2ddf9e519273927446eac6a45cbcefc049a6b349
Edward Mehr <s.r.mehr@gmail.com>
2012-02-24 02:42:05 -0500

    some new functinality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -68,6 +68,19 @@ $(document).ready(function(){
     $(this).addClass('active');
     $('body').addClass('loading');
   });
+
+  $('.slide-out-div').tabSlideOut({
+      tabHandle: '.handle',                              //class of the element that will be your tab
+      pathToTabImage: 'images/knob.jpg',          //path to the image for the tab (optionaly can be set using css)
+      imageHeight: '122px',                               //height of tab image
+      imageWidth: '40px',                               //width of tab image    
+      tabLocation: 'right',                               //side of screen where tab lives, top, right, bottom, or left
+      speed: 300,                                        //speed of animation
+      action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+      topPos: '200px',                                   //position from the top
+      fixedPosition: true                               //options: true makes it stick(fixed position) on scroll
+  });
+
 });
 
 $(document).endlessScroll({
