0e607e502e663ff8aa1d33ddb345cd8c94d39260
Edward Mehr <s.r.mehr@gmail.com>
2012-02-24 22:43:06 -0500

    new stuff

--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -5,14 +5,14 @@ class SongController < ApplicationController
     @page =  params[:page] || 1
     @after = params[:after] || 2.days.ago
     @type = params[:type] || "grid"
-    @tempo_min = params[:tempo_min]
-    @tempo_max = params[:tempo_max]
-    @danceability_min = params[:danceability_min]
-    @danceability_max = params[:danceability_max]
-    @energy_min = params[:energy_min]
-    @energy_max = params[:energy_max]
-    @loudness_min = params[:loudness_min]
-    @loudness_max = params[:loudness_max]
+    @tempo_min = params[:tempo_min] | 0.0
+    @tempo_max = params[:tempo_max] | 300.0
+    @danceability_min = params[:danceability_min] | 0.0
+    @danceability_max = params[:danceability_max] | 1.0
+    @energy_min = params[:energy_min] | 0.0
+    @energy_max = params[:energy_max] | 1.0
+    @loudness_min = params[:loudness_min] | -100.0
+    @loudness_max = params[:loudness_max] | 1.0
     @from_friends = params[:from_friends] || str_to_bool(params[:from_friends]) || false
     if @from_friends
       @user = current_user || Facebook.find_by_email(params[:user_email])
