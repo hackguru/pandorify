43e8df8234785c6b90c413493c483674a21cb2aa
Edward Mehr <s.r.mehr@gmail.com>
2012-02-25 20:15:29 -0500

    removing jquery

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -3,50 +3,46 @@ class PartyController < ApplicationController
 
   def index
     
-    if !params[:friend_tokens] || params[:friend_tokens].size == 0
-      @noSongs = true;
-    else
-      @tempo_min = params[:tempo_min] || 0.0
-      @tempo_max = params[:tempo_max] || 300.0
-      @danceability_min = params[:danceability_min] || 0.0
-      @danceability_max = params[:danceability_max] || 1.0
-      @energy_min = params[:energy_min] || 0.0
-      @energy_max = params[:energy_max] || 1.0
-      @loudness_min = params[:loudness_min] || -100.0
-      @loudness_max = params[:loudness_max] || 1.0
+    @tempo_min = params[:tempo_min] || 0.0
+    @tempo_max = params[:tempo_max] || 300.0
+    @danceability_min = params[:danceability_min] || 0.0
+    @danceability_max = params[:danceability_max] || 1.0
+    @energy_min = params[:energy_min] || 0.0
+    @energy_max = params[:energy_max] || 1.0
+    @loudness_min = params[:loudness_min] || -100.0
+    @loudness_max = params[:loudness_max] || 1.0
 
-      if current_user
-        @host = current_user
-      elsif params[:host]
-        @host = Facebook.find(params[:host])
-      elsif
-        @host = Facebook.find_by_email(params[:host_email])
-      end
-
-      ids = params[:friend_tokens].split(",")
-      ids_string = "("
-      i = 0
-      while i < ids.size() - 1
-        ids_string += "#{Facebook.table_name}.id = #{ids[i]} OR "
-        i += 1
-      end
-      ids_string += "#{Facebook.table_name}.id = #{ids[i]})"
-      @page =  params[:page] || 1
-      @songs = Song.sort_based_on_common_song_count(ids_string).songs_with_tempo_range(@tempo_min,@tempo_max).songs_with_danceability_range(@danceability_min,@danceability_max).songs_with_energy_range(@energy_min,@energy_max).songs_with_loudness_range(@loudness_min,@loudness_max).paginate(:page => @page )
-
-      @party = Party.find_or_initialize_by_host_id(@host.id);
-      @party.facebooks = Facebook.find(:all,:conditions=>[ids_string])
-      Requestedsong.find(:all, :conditions => ['party_id = ? AND added is not TRUE', @party.id]).each do |request|
-        @party.songs << request.song
-        request.added = true
-        request.save!
-      end
-      @party.save
-
-
-
-      @type = params[:type] || "grid"
+    if current_user
+      @host = current_user
+    elsif params[:host]
+      @host = Facebook.find(params[:host])
+    elsif
+      @host = Facebook.find_by_email(params[:host_email])
     end
+  
+    ids = params[:friend_tokens].split(",")
+    ids_string = "("
+    i = 0
+    while i < ids.size() - 1
+      ids_string += "#{Facebook.table_name}.id = #{ids[i]} OR "
+      i += 1
+    end
+    ids_string += "#{Facebook.table_name}.id = #{ids[i]})"
+    @page =  params[:page] || 1
+    @songs = Song.sort_based_on_common_song_count(ids_string).songs_with_tempo_range(@tempo_min,@tempo_max).songs_with_danceability_range(@danceability_min,@danceability_max).songs_with_energy_range(@energy_min,@energy_max).songs_with_loudness_range(@loudness_min,@loudness_max).paginate(:page => @page )
+
+    @party = Party.find_or_initialize_by_host_id(@host.id);
+    @party.facebooks = Facebook.find(:all,:conditions=>[ids_string])
+    Requestedsong.find(:all, :conditions => ['party_id = ? AND added is not TRUE', @party.id]).each do |request|
+      @party.songs << request.song
+      request.added = true
+      request.save!
+    end
+    @party.save
+    
+      
+    
+    @type = params[:type] || "grid"
 
     respond_to do |format|
        format.js
