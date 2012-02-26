7957897071043231882ecb0cbbb425c1a97550c7
Edward Mehr <s.r.mehr@gmail.com>
2012-02-26 06:35:25 -0500

    removing jquery

--- app/controllers/playlist_controller.rb
+++ app/controllers/playlist_controller.rb
@@ -30,7 +30,6 @@ class PlaylistController < ApplicationController
   
   def play
     @type = params[:type] || "grid"
-    @page = params[:page] || 1
     @playlist = Playlist.find(params[:id])
     @songs = @playlist.songs.order("created_at DESC")
     respond_to do |format|
